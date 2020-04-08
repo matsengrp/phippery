e2c84cde4c4b69263309dc4f366fb1209db4e2e7
jgallowa07 <jaredgalloway07@gmail.com>
2020-04-07 20:17:52 -0600

    added figures and cleaned up notebook

--- scripts/sample_run.sh
+++
@@ -1,5 +0,0 @@
-DD=../empirical_data
-COUNTS=sample_peptide_counts.csv
-PMETA=peptide_metadata.csv
-SMETA=sample_metadata.csv
-./PhiP_seq_fold_analysis.py -counts ${DD}/${COUNTS} -peptide_metadata ${DD}/${PMETA} -sample_metadata ${DD}/${SMETA} -corr_th 0.90
