a8102fdcbbc873c961ff77aaaf87b511a9615a3a
jared <jaredgalloway07@gmail.com>
2020-04-13 21:58:39 -0700

    added some infrastucture

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="phippery",
+    version="0.1",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=["Click"],
+    entry_points="""
+        [console_scripts]
+        yourscript=yourpackage.scripts.yourscript:cli
+    """,
+)
