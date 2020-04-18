e38e8bf96f6cb67a367ded4eeb4003a1f22e6c0f
jared <jaredgalloway07@gmail.com>
2020-04-17 18:43:36 -0600

    added testing data structure

--- README.md
+++ README.md
@@ -6,7 +6,10 @@
 
 [Phip-seq](file:///C:/Users/Jared/AppData/Local/Temp/Mohan%20et%20al.%202018%20-%20PhIP-Seq%20characterization%20of%20serum%20antibodies%20using%20oligonucleotide-encoded%20peptidomes.pdf)
 is a powerful protocol for investigating potential anitbody targets
-(epitopes). When the adaptive immune system gives humans the gift of immunity from
+(epitopes). Phage immunoprecipitation sequencing works by synthesizing an
+protein
+
+When the adaptive immune system gives humans the gift of immunity from
 a particular pathogen, this really means the human body has hurdled/completed a series
 of complex biological ...
 
--- test/test_utils.py
+++ test/test_utils.py
@@ -6,36 +6,58 @@
 Some unit tests for phippery.utils
 """
 
+# built-in
+import os
+import sys
+
 # dependency
 import pytest
-import phippery.utils
+import xarray as xr
+import phippery.utils as utils
 
 
-def load_test_files():
-    """
-    This function simply gives the paths
-    to the following three test files.
+class TestUtils:
+    """ Object to test phippery.utils """
 
-    counts: This is a directory with some
-        tab seperated value alignment counts
-        for demultiplexed samples:
-        1.1, 1.2, 2.1, 2.2, 35.1, 35.2, 37.1
-        each with a count for peptides:
-        1,2,3,4,5.
+    # TODO
+    # can shared_datadir be accessed without passing
+    # it as a parameter?
+    # self.counts = (shared_datadir/"test_files"/"counts")
+    # self.sample_metadata = (shared_datadir/"test_files"/"sample_metadata.tsv")
+    # self.peptide_metadata = (shared_datadir/"test_files"/"peptide_metadata.tsv")
 
-    sample_metadata:
-    """
-    test_files = "../data/test_files/"
-    return (
-        f"{test_files}counts/",
-        f"{test_files}sample_metadata",
-        f"{test_files}peptide_metadata",
-    )
+    def test_files(self, shared_datadir):
+        """
+        simple test to make sure we can find our test files,
 
+        (shared_datadir/"test_files"/"counts") - is a directory,
+        with  7 files:
 
-def test_collect_merge_prune_data():
-    """
-    simple test to get things up and running
-    """
-    # counts, s_meta, p_meta =
-    assert True
+        1.1.tsv  1.2.tsv  2.1.tsv  2.2.tsv  35.1.tsv  35.2.tsv  37.1.tsv
+
+        each a name for a file containing example peptide alignment counts for
+        a sample in a phip-seq experiement. The first integer of the filename
+        is the sample number and the second number is th technical replicate.
+
+        Each file contains two columns seperated by tabs. first column
+        is peptide id's and the second is the alignment counts.
+        """
+
+        # assert os.path.exists(self.counts)
+        # assert os.path.exists(self.sample_metadata)
+        # assert os.path.exists(self.peptide_metadata)
+
+        assert os.path.exists((shared_datadir / "test_files" / "counts"))
+        assert os.path.exists((shared_datadir / "test_files" / "sample_metadata.tsv"))
+        assert os.path.exists((shared_datadir / "test_files" / "peptide_metadata.tsv"))
+
+        return None
+
+    def test_collect_sample_metadata_types(self, shared_datadir):
+        """test that we're getting the right types"""
+
+        sample_md = utils.collect_sample_metadata(
+            f"{shared_datadir}/test_files/sample_metadata.tsv"
+        )
+
+        assert type(sample_md) == xr.DataArray
