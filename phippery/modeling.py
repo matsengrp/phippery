9bb4834f4564f367ede5bd85685081251adb55b4
jgallowa07 <jaredgalloway07@gmail.com>
2020-10-20 05:48:32 -0700

    added starting alpha and beta to gampois fit

--- phippery/modeling.py
+++ phippery/modeling.py
@@ -15,7 +15,13 @@ from phippery.gampois import gamma_poisson_posterior_rates
 from phippery.gampois import mlxp_gamma_poisson
 
 
-def gamma_poisson_model(ds, trim_percentile=99.9, data_table="size_factors"):
+def gamma_poisson_model(
+    ds,
+    starting_alpha=0.8,
+    starting_beta=0.1,
+    trim_percentile=99.9,
+    data_table="size_factors",
+):
     """
     This is a wrapper function for out xarray dataset.
     The original code can be found here:
@@ -31,6 +37,10 @@ def gamma_poisson_model(ds, trim_percentile=99.9, data_table="size_factors"):
     same dimentional coordinates (peptide_id, sample_id) as the counts table.
     """
 
+    print(
+        f"Running new modeling with starting alpha = {starting_alpha}, beta = {starting_beta}"
+    )
+
     # TODO check that data of choice is in ds
     # TODO append to sample table the alpha and beta rates
 
@@ -44,7 +54,10 @@ def gamma_poisson_model(ds, trim_percentile=99.9, data_table="size_factors"):
     trimmed_means = np.ma.mean(
         np.ma.masked_greater(counts.values, upper_bound), axis=1
     ).data
-    alpha, beta = fit_gamma(trimmed_means)
+    alpha, beta = fit_gamma(
+        trimmed_means, starting_alpha=starting_alpha, starting_beta=starting_beta
+    )
     background_rates = gamma_poisson_posterior_rates(counts, alpha, beta, upper_bound)
     counts.loc[:, :] = mlxp_gamma_poisson(counts, background_rates)
     ds[f"gamma_poisson_mlxp_{data_table}"] = xr.DataArray(counts)
+    return alpha, beta
