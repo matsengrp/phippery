595c641b39fca09d929552c35bcb965d82037cf8
jgallowa07 <jaredgalloway07@gmail.com>
2021-09-24 11:41:52 -0700

    updated requirements

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup, find_packages
 
+requirements = ["click", "numpy", "pandas", "xarray", "dask", "scipy", "statsmodels"]
+dev_requirements = ["pytest", "pytest-pep8", "pytest-datadir", "pre-commit", "black"]
 
 setup(
     name="phippery",
     version="0.1",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=["click", "numpy", "pandas", "scipy", "matplotlib"],
+    install_requires=requirements,
+    extras_require={"dev": dev_requirements},
     entry_points="""
         [console_scripts]
         phippery=phippery.cli:cli
