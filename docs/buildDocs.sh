12cce7970c3471a438debc09ec0f1e4a96da1722
Jared Galloway <jaredgalloway07@gmail.com>
2021-10-18 14:55:44 -0700

    slim down dependencies

--- docs/buildDocs.sh
+++ docs/buildDocs.sh
@@ -19,8 +19,8 @@ set -x
 apt-get update
 #apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme python-pip
 apt-get -y install git rsync python3-pip
-pip3 install -r requirements.txt
-#sphinx sphinx_rtd_theme sphinx-click
+#pip3 install -r requirements.txt
+pip3 install pandas numpy sphinx sphinx_rtd_theme sphinx-click
  
 #####################
 # DECLARE VARIABLES #
