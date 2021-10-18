860ca36a24834e5434585f48b223490ddbf1b9ce
Jared Galloway <jaredgalloway07@gmail.com>
2021-10-18 14:05:01 -0700

    more deps to pip

--- docs/buildDocs.sh
+++ docs/buildDocs.sh
@@ -17,8 +17,9 @@ set -x
 ###################
  
 apt-get update
-apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme python-pip
-pip install sphinx_click
+#apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme python-pip
+apt-get -y install git rsync python-pip
+pip install sphinx sphinx_rtd_theme sphinx-click
  
 #####################
 # DECLARE VARIABLES #
