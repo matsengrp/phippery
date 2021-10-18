8c2bdf5ed50c1995ab0db2a581cd7d25da9db90c
Jared Galloway <jaredgalloway07@gmail.com>
2021-10-18 14:40:21 -0700

    requirements updated for sphinx click

--- docs/buildDocs.sh
+++ docs/buildDocs.sh
@@ -19,9 +19,7 @@ set -x
 apt-get update
 #apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme python-pip
 apt-get -y install git rsync python3-pip
-
-
-pip3 install -r ../requirements.txt
+pip3 install -r requirements.txt
 #sphinx sphinx_rtd_theme sphinx-click
  
 #####################
