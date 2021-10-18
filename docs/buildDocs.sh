74a92d4672808993cc0b6bff7bd00e571e5abab6
Jared Galloway <jaredgalloway07@gmail.com>
2021-10-18 14:22:05 -0700

    python3 docs build

--- docs/buildDocs.sh
+++ docs/buildDocs.sh
@@ -18,7 +18,7 @@ set -x
  
 apt-get update
 #apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme python-pip
-apt-get -y install git rsync python-pip
+apt-get -y install git rsync python3-pip
 pip install sphinx sphinx_rtd_theme sphinx-click
  
 #####################
