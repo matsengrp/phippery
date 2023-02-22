eabc7a094b630169a3df22bfe158410ad93b1c63
Jared Galloway <jaredgalloway07@gmail.com>
2023-02-22 08:04:00 -0800

    move util funcs into phippery namespace

--- phippery/__init__.py
+++ phippery/__init__.py
@@ -3,11 +3,8 @@
 
 __version__ = "1.1.4"
 
-#from phippery.utils import *
-import phippery.utils
-#from phippery.normalize import *
+from phippery.utils import *
 import phippery.normalize
-#from phippery.modeling import *
 import phippery.modeling
 import phippery.eigen
 import phippery.escprof
